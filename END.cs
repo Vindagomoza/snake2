d2e9582caadcc431ca532db23c1737d2abff6f79
Vindagomoza <loopofhorror@gmail.com>
2020-08-27 21:55:33 +0300

    test 2
    snake 15 loeng test 2

---
+++ END.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class END
+    {
+        int x;
+        int y;
+        int scr;
+        public END(int x, int y, int scr)
+        {
+            this.x = x;
+            this.y = y;
+            this.scr = scr;
+        }
+        public void DrawEnd()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(x, y);
+            Console.Write("####GAME OVER####");
+            Console.SetCursorPosition(x+6, y+1);
+            Console.Write("Press any button to continue");
+            Console.SetCursorPosition(x+3, y + 2);
+            Console.Write("Score:{0}", scr);
+        }
+    }
+}
